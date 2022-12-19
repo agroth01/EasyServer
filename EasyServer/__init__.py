a02ba8001d25cac406dbe9565b323419311a3971
agroth01 <85568620+agroth01@users.noreply.github.com>
2022-12-19 16:45:09 +0900

    added package files

--- EasyServer/__init__.py
+++ EasyServer/__init__.py
@@ -1,2 +1,13 @@
+"""
+Easyserver.
+
+An easy high level way to create a client/server application.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Alexander Groth'
+__credits__ = 'Alexander Groth'
+
+
 from easyserver.server.easyserver import EasyServer
 from easyserver.client.easyclient import EasyClient
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='EasyServer',
+    version='0.1.0',    
+    description='An easy high level way to create a client/server application',
+    url='https://github.com/agroth01/EasyServer',
+    author='Alexander Groth',
+    author_email='software.agroth@gmail.com',
+    license='APACHE 2.0',
+    packages=['easyserver'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Hobbyists',
+        'License :: APACHE 2.0',  
+        'Operating System :: POSIX :: Linux :: Windows',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
