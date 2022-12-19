ee3d62724a3793c9a6bba1e177ccfc442fc24841
agroth01 <85568620+agroth01@users.noreply.github.com>
2022-12-19 18:21:33 +0900

    added server callbacks

--- EasyServer/client/easyclient.py
+++ EasyServer/client/easyclient.py
@@ -1,3 +1,50 @@
+import socket
+import time
+
 class EasyClient:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, host, port) -> None:
+        self.host = host
+        self.port = port
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setblocking(0)
+
+    def connect(self) -> None:
+        """
+        Connects to the server.
+        """
+        self.socket.connect((self.host, self.port))
+
+    def send_message(self, message) -> None:
+        """
+        Sends a message to the server.
+        """
+        self.socket.send(message.encode())
+
+    def read_message(self) -> str:
+        """
+        Reads a message from the server. Will return None
+        if no message is available at the time of calling.
+        """
+        return self.socket.recv(1024).decode()
+
+    def wait_for_message(self, timeout=0) -> str:
+        """
+        Reads a message from the server. 
+        Will block thread until a message is received.
+        Has optional timeout parameter before returning None.
+        """
+        start_time = time.time()
+        while True:
+            message = self.read_message()
+            if message:
+                return message
+            else:
+                if timeout == 0:
+                    continue
+
+                elapsed_time = time.time() - start_time
+                if elapsed_time > timeout:
+                    break
+        return None
+        
--- EasyServer/server/easyserver.py
+++ EasyServer/server/easyserver.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 import EasyServer.server.defaults as DefaultValues
 from EasyServer.server.client_connection import ClientConnection
 from EasyServer.networking.network_message import NetworkMessage
@@ -9,17 +10,23 @@ class EasyServer:
     A high level server class that handles the connection of clients and transportation
     of data between the server and the clients.
     """
-    def __init__(self, host, port, tick_rate=DefaultValues.TICK_RATE, buffer_size=DefaultValues.BUFFER_SIZE) -> None:
+    def __init__(self, host, port, tick_rate=DefaultValues.TICK_RATE, buffer_size=DefaultValues.BUFFER_SIZE,
+                 manual_mode=False) -> None:
         self.host = host
         self.port = port
         self.tick_rate = tick_rate
         self.buffer_size = buffer_size
+        self.manual_mode = manual_mode
+
+        # set up callbacks
+        self.__on_message_callback = self.on_message
+        self.__on_connection_callback = self.on_connection
+        self.__on_disconnect_callback = self.on_disconnect
 
         self.__clients = []
         self.__stop_flag = False
 
     # starting and stopping the server
-
     def start(self) -> None:
         """
         Starts the server. This is a blocking call.
@@ -41,6 +48,42 @@ class EasyServer:
         self.socket.bind((self.host, self.port))
         self.socket.listen(0)
 
+        # start main loop if not in manual mode
+        if not self.manual_mode:
+            self.__server_loop()
+
+    # server loop
+
+    def update(self):
+        """
+        Repeatedly called by the server loop. Can also be called externally to update the server
+        if set to manual mode.
+        """
+        start_time = time.time()
+
+        #self.__accept_connections()
+        #self.__receive_messages()
+
+        # ignore keeping the server at a constant tick rate if in manual mode, as user is responsible for calling update.
+        if not self.manual_mode:
+            self.__tick(start_time)
+
+    def __server_loop(self) -> None:
+        """
+        Internal method that handles the server loop.
+        """
+        while not self.__stop_flag:
+            self.update()
+
+    def __tick(self, start_time: float) -> None:
+        """
+        Attempts to keep the server running at a constant tick rate.
+        """
+        elapsed_time = time.time() - start_time
+        sleep_time = 1 / self.tick_rate - elapsed_time
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+
     # Connection events
 
     def on_connection(self, client: ClientConnection) -> None:
@@ -61,20 +104,38 @@ class EasyServer:
         """
         pass
 
+    def set_connection_callback(self, callback) -> None:
+        """
+        Sets the callback to be invoked when a client connects to the server.
+        """
+        self.__on_connection_callback = callback
+
+    def set_message_callback(self, callback) -> None:
+        """
+        Sets the callback to be invoked when a message is received from a client.
+        """
+        self.__on_message_callback = callback
+
+    def set_disconnect_callback(self, callback) -> None:
+        """
+        Sets the callback to be invoked when a client disconnects from the server.
+        """
+        self.__on_disconnect_callback = callback
+
     def __on_connection(self, client: ClientConnection) -> None:
         """
         Internal method that handles the connection of a client.
         Will store the connection in a list and invoke event.
         """
         self.__clients.append(client)
-        self.on_connection(client)
+        self.__on_connection_callback(client)
 
     def __on_message(self, message: NetworkMessage, client: ClientConnection) -> None:
         """
         Internal method that handles the message of a client.
         Will invoke event.
         """
-        self.on_message(message, client)
+        self.__on_message_callback(message, client)
 
     def __on_disconnect(self, client: ClientConnection) -> None:
         """
@@ -82,4 +143,4 @@ class EasyServer:
         Will remove the connection from the list and invoke event.
         """
         self.__clients.remove(client)
-        self.on_disconnect(client)
+        self.__on_disconnect_callback(client)
